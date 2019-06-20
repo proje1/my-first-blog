62a363ea9b56b91ba383aec9ec07f5091dce9afd
proje1 <projemaili18@gmail.com>
2019-06-20 16:43:44 +0300

    Sitede gönderi oluşturmak ve düzenlemek için view'ler eklendi.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,4 @@
 """mysite URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -19,4 +18,5 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    
 ]
